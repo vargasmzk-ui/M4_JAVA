c77bee8b6d561bb10be7e155a9853d7f5649346e
vargasmzk-ui <vargas.mzk@gmail.com>
2026-08-13 15:56:31 +0000

    S04

--- S04/Main.java
+++ S04/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Persona p = new Persona(nombre:"Eric, edad:25");
-        //p.mostrarDatos();
-        p.setEdad(26);
-        
+        // Creamos el objeto de tipo persona, indicando que tiene 25 años
+        Persona p = new Persona("Eric", 25);
+        // Mostramos los datos de la persona
+        p.mostrarDatos();
+        // Modificamos la edad de la persona a 30 años
+        p.setEdad(30);
+        // Mostramos los datos de la persona con la edad actualizada
+        System.out.println("Después de 5 años, ahora " + p.getNombre() + " tiene " + p.getEdad() + " años.");
     }
-    
 }
+
--- S04/Persona.java
+++ S04/Persona.java
@@ -1,38 +1,41 @@
 public class Persona {
-    
+   
+    // Declaración de atributos privados
     private String nombre;
     private int edad;
 
-    //constructor
-    public Persona (String nombre, int edad) {
-        this. nombre = nombre;
-        this.edad = edad;
 
+    // Constructor
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
     }
 
-    //metodo set
+
+    // Método get
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    // Método get
+    public int getEdad() {
+        return edad;
+    }
+
+
+    // Método set
     public int setEdad(int nuevaEdad) {
         return edad = nuevaEdad;
     }
 
-    //metodo get
-    public string getNombre () {
-        return nombre;
-    }
 
-    //metodo get
-    public int setEdad (int nuevaEdad) {
-        return edad = nuevaEdad;
-
-    }
-
+    // Método normal
     void mostrarDatos() {
-        System.out.println("=".repeat(count:20));
         System.out.println("=".repeat(20));
         System.out.println("ACTIVIDAD ENCAPSULAMIENTO");
+        System.out.println("=".repeat(20));
         System.out.println("Nombre: " + nombre + "\nEdad: " + edad);
         System.out.println("=".repeat(20));
-
     }
-
 }
